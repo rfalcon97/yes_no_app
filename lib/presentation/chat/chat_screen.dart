557fb59031bb0ddd8a2b8836c0c087c35e5121f9
Ricardo Falcon <rfalcon@29deoctubre.fin.ec>
2025-12-30 18:21:18 -0500

    capitulo 70 50%

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://img.dreamkitsapp.com/players/dls26/382.webp',
+            ),
+          ),
+        ),
+        title: const Text('CR7'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(itemBuilder: (context, index) {
+              return  Text('Indice $index');
+            })),
+            Text('¿Cómo estás?'),
+          ],
+        ),
+      ),
+    );
+  }
+}
